4d7909e0847985d528ea1cc6457a32c7de975e4c
Alex <aeversme@gmail.com>
2021-02-02 16:32:10 -0500

    use a splice to change the movie list

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import requests
 
 response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
 
-best_movies_webpage = response.text
-soup = BeautifulSoup(best_movies_webpage, 'html.parser')
+website_html = response.text
+soup = BeautifulSoup(website_html, 'html.parser')
 movie_titles = soup.find_all(name='h3', class_='title')
 
 reverse_movie_list = [movie.getText() for movie in movie_titles]
-print(reverse_movie_list)
 
-with open('movies.txt', 'a', encoding='utf-8') as file:
-    for i in range(len(reverse_movie_list)):
-        file.write(f'{reverse_movie_list.pop()}\n')
+# My initial solution:
+
+# with open('movies.txt', 'a', encoding='utf-8') as file:
+#     for i in range(len(reverse_movie_list)):
+#         file.write(f'{reverse_movie_list.pop()}\n')
+
+# Alternate method:
+
+movie_list = reverse_movie_list[::-1]
+
+with open('movies.txt', 'w', encoding='utf-8') as file:
+    for movie in movie_list:
+        file.write(f'{movie}\n')
