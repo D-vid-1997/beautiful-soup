8893b33bf973e98bfbcbd2f6bfeb54b777013ac5
Alex <aeversme@gmail.com>
2021-02-02 16:17:36 -0500

    move ycombinator scrape to test and scrape a movie list

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get('https://news.ycombinator.com/news')
+response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
 
-yc_webpage = response.text
+best_movies_webpage = response.text
+soup = BeautifulSoup(best_movies_webpage, 'html.parser')
+movie_titles = soup.find_all(name='h3', class_='title')
 
-soup = BeautifulSoup(yc_webpage, 'html.parser')
+reverse_movie_list = [movie.getText() for movie in movie_titles]
+print(reverse_movie_list)
 
-articles = soup.find_all(name='a', class_='storylink')
-article_text = [article.getText() for article in articles]
-article_links = [article.get('href') for article in articles]
-
-upvotes_text = [int(upvotes.getText().split()[0]) for upvotes in soup.find_all(name='span', class_='score')]
-
-# print(article_text)
-# print(article_links)
-# print(upvotes_text)
-
-# print(int(upvotes_text[0].split()[0]))
-
-
-def get_index_of_greatest_item_value(list_of_int_values):
-    highest_value = max(list_of_int_values)
-    index_of_highest_item_value = list_of_int_values.index(highest_value)
-    return index_of_highest_item_value
-
-
-print(upvotes_text)
-print(get_index_of_greatest_item_value(upvotes_text))
-
-highest_value_index = get_index_of_greatest_item_value(upvotes_text)
-print(article_text[highest_value_index])
-print(article_links[highest_value_index])
+with open('movies.txt', 'a', encoding='utf-8') as file:
+    for i in range(len(reverse_movie_list)):
+        file.write(f'{reverse_movie_list.pop()}\n')
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+# import requests
 
-with open("website.html") as file:
-    contents = file.read()
+# with open("website.html") as file:
+#     contents = file.read()
 
-soup = BeautifulSoup(contents, 'html.parser')
+# soup = BeautifulSoup(contents, 'html.parser')
 
 # print(soup.title)
 # print(soup.title.name)
@@ -43,3 +44,35 @@ soup = BeautifulSoup(contents, 'html.parser')
 #
 # headings = soup.select(selector='.heading')
 # print(headings)
+
+# response = requests.get('https://news.ycombinator.com/news')
+
+# yc_webpage = response.text
+
+# soup = BeautifulSoup(yc_webpage, 'html.parser')
+
+# articles = soup.find_all(name='a', class_='storylink')
+# article_text = [article.getText() for article in articles]
+# article_links = [article.get('href') for article in articles]
+
+# upvotes_text = [int(upvotes.getText().split()[0]) for upvotes in soup.find_all(name='span', class_='score')]
+
+# print(article_text)
+# print(article_links)
+# print(upvotes_text)
+
+# print(int(upvotes_text[0].split()[0]))
+
+
+# def get_index_of_greatest_item_value(list_of_int_values):
+#     highest_value = max(list_of_int_values)
+#     index_of_highest_item_value = list_of_int_values.index(highest_value)
+#     return index_of_highest_item_value
+
+
+# print(upvotes_text)
+# print(get_index_of_greatest_item_value(upvotes_text))
+
+# highest_value_index = get_index_of_greatest_item_value(upvotes_text)
+# print(article_text[highest_value_index])
+# print(article_links[highest_value_index])
